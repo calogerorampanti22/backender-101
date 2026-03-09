c3e19f1bb8ea3d53b49de761e8b8f90ba539ea15
Calogero Rampanti <calogerorampanti7@gmail.com>
2026-03-09 15:54:05 +0100

    Esercizio 1
    Creazione repository, IntelliJ, Spring boot tutorial e Postman

---
+++ src/main/java/calogero/rampanti/backender_101/service/PersonService.java
@@ -0,0 +1,42 @@
+package calogero.rampanti.backender_101.service;
+
+import calogero.rampanti.backender_101.dao.PersonDao;
+import calogero.rampanti.backender_101.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("postgres") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person newPerson) {
+        return personDao.updatePersonById(id, newPerson);
+    }
+
+    public int deletePerson(UUID id) {
+        return personDao.deletePersonById(id);
+    }
+}
