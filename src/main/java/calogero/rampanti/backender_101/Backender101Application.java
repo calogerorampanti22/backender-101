b05b8bba67f9a598be95e3644fe43b5049cbeaf0
Calogero Rampanti <calogerorampanti7@gmail.com>
2026-03-09 11:50:17 +0100

    Progetto creato

---
+++ src/main/java/calogero/rampanti/backender_101/Backender101Application.java
@@ -0,0 +1,13 @@
+package calogero.rampanti.backender_101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backender101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backender101Application.class, args);
+	}
+
+}
---
+++ src/test/java/calogero/rampanti/backender_101/Backender101ApplicationTests.java
@@ -0,0 +1,13 @@
+package calogero.rampanti.backender_101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backender101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
